3309dfab5c9966c9deb83fe92e295f769e984a7e
Diego Massagli <diegomassagli@gmail.com>
2021-09-07 16:24:06 -0300

    con video 341 completo

---
+++ components/layout/Navegacion.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+import styled from '@emotion/styled';
+
+const Nav = styled.nav`
+  padding-left: 2rem;
+
+  a {
+      font-size: 1.8rem;
+      margin-left: 2rem;
+      color: var(--gris2);
+      font-family: 'PT Sans', serif;
+
+      &:last-of-type {
+        margin-right: 0;
+      }
+  }
+`;
+
+const Navegacion = () => {
+  return (
+    <Nav>
+        <Link href="/"> Inicio </Link>
+        <Link href="/"> Populares </Link>
+        <Link href="/"> Nuevo Producto </Link>
+
+    </Nav>
+
+    );
+}
+ 
+export default Navegacion;
