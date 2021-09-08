1b2f63f667c8d82feeeb54a824de10436fd8b977
Diego Massagli <diegomassagli@gmail.com>
2021-09-08 17:20:08 -0300

    casi completo el video 353, agregue firebase y no funciona. antes de reinstalar otra version de firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCWRV1Xx9WM8iA3JmzjdoGfr-ajyAg8mM",
+  authDomain: "producthunt2-c6bd8.firebaseapp.com",
+  projectId: "producthunt2-c6bd8",
+  storageBucket: "producthunt2-c6bd8.appspot.com",
+  messagingSenderId: "3985312095",
+  appId: "1:3985312095:web:d352996b38ac9fd20e4eea"
+};
+
+export default firebaseConfig;
+
